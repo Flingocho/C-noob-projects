f8dd3b47f987c8eae18adeac6c08ee75cd905738
Flingocho <jaimevidalt.24@gmail.com>
2024-05-09 18:10:21 +0200

    Se implementa la función de rotación. Falta inputs teclado

--- alpha-tetris.c
+++ alpha-tetris.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
-
+//CONSTANTES
 #define FILAS 10
 #define COLUMNAS 10
-
-enum Movimientos {
-    ABAJO = 1,
-    DERECHA = 2,
-    IZQUIERDA = 3,
-    ROTAR = 4
+//movimientos
+enum Movement {
+    DOWN = 1,
+    RIGHT = 2,
+    LEFT = 3,
+    ROTATE = 4
 };
-
+//función pintar pantalla
 void print_screen(char screen[FILAS][COLUMNAS]) {
+    //imprime el estado de screen (ver move_piece()*1)
     printf("Matriz:\n");
-    for (int i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
-            printf("%c ", screen[i][j]);
+    for(int i = 0; i < FILAS; i++) {
+        for(int j = 0; j < COLUMNAS; j++) {
+            printf("%c", screen[i][j]);
         }
         printf("\n");
     }
 }
+//función para mover las piezas
+void move_piece(enum Movement movement, char screen[FILAS][COLUMNAS], int *rotation){
 
-void mover_pieza(char screen[FILAS][COLUMNAS], enum Movimientos movimiento) {
-    char nueva_screen[FILAS][COLUMNAS] = {
+    //variables locales
+    //Pantalla en blanco para operar
+    char new_screen[FILAS][COLUMNAS] ={
         {'O','O','O','O','O','O','O','O','O','O'},
         {'O','O','O','O','O','O','O','O','O','O'},
         {'O','O','O','O','O','O','O','O','O','O'},
@@ -33,45 +37,84 @@ void mover_pieza(char screen[FILAS][COLUMNAS], enum Movimientos movimiento) {
         {'O','O','O','O','O','O','O','O','O','O'},
         {'O','O','O','O','O','O','O','O','O','O'},
     };
+    //X a mover de la pieza
+    int rotation_item = 0;
+    //rotaciones entre estados
+    int rotations[4][4][2] ={
+            {{1, 1}, {0, 0}, {-2, 0}, {-1, -1}},
+            {{0, 1}, {-1, 0}, {0, -1}, {1, -2}},
+            {{0, 2}, {1, 1}, {-1, 1}, {-2, 0}},
+            {{0, 1}, {1, 0}, {2, -1}, {1, -2}},
+        };
+    //damos a new rotation el valor de *rotation (puntero)
+    int new_rotation;
+    new_rotation = *rotation;
 
-    
-        
+    //en caso de rotación se incrementa el estado de rotation 0-3
+    if(movement == ROTATE){
+        new_rotation = new_rotation + 1;
+    //si llegamos a 4 volvemos a 0
+            if(new_rotation == 4){
+                new_rotation = 0;
+            }
+    }
+    //paseamos todas las filas y columnas
+    for(int row_index = 0; row_index < FILAS; row_index++){
+        for(int column_index = 0; column_index < COLUMNAS; column_index++ ){
+            //si el valor de la casilla es X
+            if(screen[row_index][column_index] == 'X'){
+                //se define un nuevo índice cada vez que lo queremos mover
+                int new_row_index = 0;
+                int new_column_index = 0;
+                //operativa lógica movimientos
+                switch (movement)
+                {
+                    case DOWN:
+                        new_row_index = row_index +1;
+                        new_column_index = column_index;
+                        break;
 
+                    case RIGHT:
+                        new_row_index = row_index;
+                        new_column_index = column_index + 1;
+                        break;
 
-    for (int fila_actual = 0; fila_actual < FILAS; fila_actual++) {
-        for (int columna_actual = 0; columna_actual < COLUMNAS; columna_actual++) {
-            if (screen[fila_actual][columna_actual] == 'X') {
-                int nueva_fila = fila_actual;
-                int nueva_columna = columna_actual;
-                switch (movimiento) {
-                    case ABAJO:
-                        nueva_fila = fila_actual + 1;
+                    case LEFT:
+                        new_row_index = row_index;
+                        new_column_index = column_index - 1;
                         break;
-                    case DERECHA:
-                        nueva_columna = columna_actual + 1;
-                        break;
-                    case IZQUIERDA:
-                        nueva_columna = columna_actual - 1;
-                        break;
-                    case ROTAR:
-                        printf("Rotar\n");
+
+                    case ROTATE:
+                
+                        //estado rotacion, pieza a rotar, fila(0) o columna(1)
+                        new_row_index = row_index + rotations[new_rotation][rotation_item][0];
+                        new_column_index = column_index + rotations[new_rotation][rotation_item][1];
+                        rotation_item = rotation_item + 1;
                         break;
                 }
-                nueva_screen[nueva_fila][nueva_columna] = 'X';
+                //metemos la X en cada vuelta del bucle que corresponda
+                new_screen[new_row_index][new_column_index] = 'X';
             }
         }
     }
-    // Actualizar la matriz screen con los valores de nueva_screen
+    //salida de los bucles
+    *rotation = new_rotation;
+    //damos los valores a screen de new screen
     for (int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLUMNAS; j++) {
-            screen[i][j] = nueva_screen[i][j];
+            screen[i][j] = new_screen[i][j];
         }
     }
-    print_screen(screen);
-}
 
+    //imprimimos estado de la pantalla tras el movimiento
+    //(*1)En C se pasan los arrays de char como punteros 
+    //así que print "screen" o "new screen" dará el mismo resultado
+    //diremos new_screen por legibilidad 
+    print_screen(new_screen);
+}
+//cuerpo principal e inicio del programa
 int main() {
-    // Matriz de pantalla
+    //pantalla inicial (pieza)
     char screen[FILAS][COLUMNAS] = {
         {'X','O','O','O','O','O','O','O','O','O'},
         {'X','X','X','O','O','O','O','O','O','O'},
@@ -84,11 +127,18 @@ int main() {
         {'O','O','O','O','O','O','O','O','O','O'},
         {'O','O','O','O','O','O','O','O','O','O'},
     };
-    
+    //estado de rotación inicial
+    int rotation = 0;
+    //puntero de rotation como parámetro para guardar cambios de estado
+    int *p_rotation;
+    p_rotation = &rotation;
+    //print pantalla inicial
     print_screen(screen);
-    mover_pieza(screen, ABAJO);
-    mover_pieza(screen, DERECHA);
-    mover_pieza(screen, DERECHA);
-    mover_pieza(screen, ABAJO);
+    //movimientos 
+    //**Implementarlos por teclado en prox version**//
+    move_piece(ROTATE, screen, p_rotation);
+    move_piece(DOWN, screen, p_rotation);
+    move_piece(ROTATE, screen, p_rotation);
+    move_piece(ROTATE, screen, p_rotation);
     return 0;
 }
